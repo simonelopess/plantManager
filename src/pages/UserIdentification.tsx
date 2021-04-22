f9e9dacaf864247b72793ac918be15b059a6338e
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-04-22 01:36:39 -0300

    Desabilitando teclado ao clicar na tela

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, TextInput, KeyboardAvoidingView, Platform} from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, TextInput, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard} from 'react-native';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
@@ -31,6 +31,7 @@ export function UserIdentification() {
     return (
         <SafeAreaView style={styles.container}>
             <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}> 
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <View style={styles.content}>
                 <View style={styles.form}>
                     <View style={styles.header}>
@@ -45,6 +46,7 @@ export function UserIdentification() {
                     </View>
                 </View>
             </View>
+            </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
