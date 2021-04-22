77f495f3a67c7d0959b2833baadf81ca18d77405
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-04-21 22:15:19 -0300

    Configuração de font

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   SafeAreaView,
+  View,
   Text,
   Image,
   StyleSheet,
@@ -10,24 +11,25 @@ import {
 
 import wateringImg from "../assets/watering.png";
 import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 import { Feather } from "@expo/vector-icons";
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {"\n"} suas plantas {"\n"} de forma fácil
-      </Text>
-      <Image source={wateringImg} style={styles.image} resizeMode="contain" />
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
-      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
-        <Text style={styles.buttonText}>
-          <Feather name="chevron-right" style={styles.buttonIcon} />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"} suas plantas de {"\n"} forma fácil
         </Text>
-      </TouchableOpacity>
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
+        <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -35,21 +37,28 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "space-around",
+    paddingHorizontal: 20,
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: "bold",
     textAlign: "center",
     color: colors.heading,
     marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   subtitle: {
     textAlign: "center",
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
   image: {
     height: Dimensions.get("window").width * 0.7,
