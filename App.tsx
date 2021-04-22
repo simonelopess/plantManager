5d57bfd14c6f3083f01c48ee5b7e8dc980bd0cce
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-04-22 00:18:24 -0300

    User Identification page criação

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 import { Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 
@@ -15,6 +15,6 @@ export default function App() {
    <AppLoading/>
   )
   return (
-    <Welcome />
+    <UserIdentification />
   )
 }
