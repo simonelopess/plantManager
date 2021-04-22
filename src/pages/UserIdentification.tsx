0589595e432f1f49d0117e4423450fc1ab75c23e
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-04-22 00:35:12 -0300

    Tela de confirmação de dados condicional para personalização de tela

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet, View, Text, TextInput, KeyboardAvoidingView, Platform} from 'react-native';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
@@ -6,16 +6,35 @@ import fonts from '../styles/fonts';
 import {Button} from '../components/Button';
 
 export function UserIdentification() {
+    const [isFocused, setFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+    const [name, setName] = useState<string>();
+    
+    function handleInputBlur() {
+        setFocused(false);
+        setIsFilled(!!name);
+    }
+    function handleInputFocus() {
+        setFocused(true)
+    }
+
+    function handleInputChange(value: string) {
+        setIsFilled(!!value)
+        setName(value)
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}> 
             <View style={styles.content}>
                 <View style={styles.form}>
-                    <Text style={styles.emoji}>
-                    😃
-                    </Text>
-                    <Text style={styles.title}>Como podemos {'\n'} chamar você?</Text>
-                    <TextInput style={styles.input} placeholder="Digite um nome" />
+                    <View style={styles.header}>
+                        <Text style={styles.emoji}>
+                        {isFilled ? '😄' : '😃' }
+                        </Text>
+                        <Text style={styles.title}>Como podemos {'\n'} chamar você?</Text>
+                    </View>
+                    <TextInput style={[styles.input, (isFocused ||  isFilled) && {borderColor: colors.green}]} placeholder="Digite um nome" onBlur={handleInputBlur} onFocus={handleInputFocus} onChangeText={handleInputChange}/>
                     <View style={styles.footer}>
                     <Button/>
                     </View>
@@ -41,6 +60,9 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         paddingHorizontal: 54,
+        alignItems: 'center'
+    },
+    header: {
         alignItems: 'center'
     },
     emoji: {
